8c3b0de79911428376cb068397a92b9a934cb3fb
Sameer <sameer.libang@gmail.com>
2020-08-12 22:42:35 +1000

    making production container SSHable

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,5 @@
+echo $SSH_PUBLIC_KEY > ~/.ssh/authorized_keys
+
 bundle exec rake db:migrate 2>/dev/null || bundle exec rake db:create db:migrate
 echo 'Postgres database has been created and migrated!'
 
