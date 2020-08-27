fab3a40b1e835d05b5a2673f87cbaf0bafc9bc33
alliesground <sameer.libang@gmail.com>
2020-08-27 14:41:12 +1000

    removing posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_24_132659) do
+ActiveRecord::Schema.define(version: 2020_08_27_041505) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
