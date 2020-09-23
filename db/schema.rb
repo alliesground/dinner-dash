ea4514a81b6ff0acbc005214baf5700e2a0178c3
alliesground <sameer.libang@gmail.com>
2020-09-23 10:43:55 +1000

    migration for orders table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_22_130759) do
+ActiveRecord::Schema.define(version: 2020_09_22_153146) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,6 +76,11 @@ ActiveRecord::Schema.define(version: 2020_09_22_130759) do
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "active_for_sale", default: true
     t.index ["title"], name: "index_items_on_title", unique: true
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
