ad606088d4d4fa7cf96e566ba551a8008b587ea9
alliesground <sameer.libang@gmail.com>
2020-10-18 20:54:19 +1100

    initializing and displaying cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :get_cart
+
   def set_flash_message(msg)
     response.set_header('Flash-Message',msg ) 
+  end
+
+  private
+
+  def get_cart
+    @cart = Cart.find(session[:cart_id])
+  rescue ActiveRecord::RecordNotFound
+    @cart = Cart.create
+    session[:cart_id] = @cart.id
   end
 end
