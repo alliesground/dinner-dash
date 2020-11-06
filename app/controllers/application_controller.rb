c539d3f3ff37f2f3f7657f05f6bfa042e35ce4c5
alliesground <sameer.libang@gmail.com>
2020-11-06 11:44:44 +1100

    refactoring application_controller#before_action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,9 @@
 class ApplicationController < ActionController::Base
   include CartSessionHelper
 
-  before_action :get_cart
+  before_action :current_cart
 
   def set_flash_message(msg)
     response.set_header('Flash-Message',msg ) 
-  end
-
-  private
-
-  def get_cart
-    current_cart
   end
 end
